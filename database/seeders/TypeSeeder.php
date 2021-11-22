4019131a2ff5cc1abd8f373ee301aa22d6d1c2a5
striderxfossility <y%^w7W%8Hc%t>
2021-11-22 10:09:41 +0100

    Added Type Model
    Signed-off-by: striderxfossility <y%^w7W%8Hc%t>

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Type;
+
+class TypeSeeder extends Seeder
+{
+    private function create(string $headType, string $name, string $code = '')
+    {
+        Type::create([
+            'type_id'   => Type::whereName($headType)->first() ? Type::whereName($headType)->first()->id : 0,
+            'name'      => $name,
+            'code'      => $code
+        ]);
+    }
+
+    public function run()
+    {
+    }
+}
