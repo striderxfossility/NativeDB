9e5da979f0c603efdbc7f1cb94d5a9a9bab0e2d8
striderxfossility <y%^w7W%8Hc%t>
2021-11-24 17:32:53 +0100

    nice void return
    Signed-off-by: striderxfossility <y%^w7W%8Hc%t>

--- resources/views/pages/types/show.blade.php
+++ resources/views/pages/types/show.blade.php
@@ -25,7 +25,7 @@
                             {{ $prop->name }} 
                         </div>
                         <div class="">
-                            : 
+                            return :
                             @if(str_contains($prop->return, 'array'))
                                 <span class="text-green-700">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -84,7 +84,17 @@
                         @endif
                     </div>
                     <div class="">
-                        : 
+                        @if($method->return == '')
+                            <div class="text-yellow-700">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.428 15.428a2 2 0 00-1.022-.547l-2.387-.477a6 6 0 00-3.86.517l-.318.158a6 6 0 01-3.86.517L6.05 15.21a2 2 0 00-1.806.547M8 4h8l-1 1v5.172a2 2 0 00.586 1.414l5 5c1.26 1.26.367 3.414-1.415 3.414H4.828c-1.782 0-2.674-2.154-1.414-3.414l5-5A2 2 0 009 10.172V5L8 4z" />
+                                </svg>    
+                                void
+                            </div>
+                        @else
+                            return : 
+                        @endif
+                        
                         @if(str_contains($method->return, 'array'))
                             <span class="text-green-700">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
