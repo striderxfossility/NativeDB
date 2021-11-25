da082d0e2205d0827eb89c394313f0fc83768fc3
striderxfossility <y%^w7W%8Hc%t>
2021-11-25 17:43:00 +0100

    replace
    Signed-off-by: striderxfossility <y%^w7W%8Hc%t>

--- app/Models/Prop.php
+++ app/Models/Prop.php
@@ -32,14 +32,17 @@ class Prop extends Model
 
     public function getReturnNiceAttribute()
     {
-        $strBuilder = '<span class="text-green-700">';
-
-        $strBuilder .= '<svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
-                        </svg> ';
-
-        $strBuilder .= $this->return . '</span>';
+        $replace = '<span class="text-green-700">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10" />
+                            </svg> Array :
+                        </span>';
+                    
         
+
+        $strBuilder = $this->return;
+
+        $strBuilder = str_replace("array:", $replace, $strBuilder);
 
         return $strBuilder;
     }
