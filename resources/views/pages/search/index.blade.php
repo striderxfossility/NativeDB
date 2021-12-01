5fc560a45f75abf49feb3ae088e73fe2db7f5ff7
striderxfossility <y%^w7W%8Hc%t>
2021-12-01 14:42:31 +0100

    remove css code from search

--- resources/views/pages/search/index.blade.php
+++ resources/views/pages/search/index.blade.php
@@ -54,38 +54,7 @@
             <div id="blockLandingStimBroadcasting" class="mb-3 rounded overflow-hidden">
                 @foreach ($natives as $native)
                     <div onclick="window.location.href = '{{ route('types.show', \App\Models\Type::getType($native->type)) }}';" class="hover:bg-gray-100 px-10 cursor-pointer">
-                        <div class="">
-                            <span class="text-yellow-600">{{ $native->type }} => {{ $native->method }}</span> 
-                            <div class="code pb-2 pt-2 w-auto" style="position:relative;">
-                                <button onclick="copyCodeNative{{ $native->id }}()" title="Copy" class="p-2 absolute top-2 right-0" style="background-color: #0d1117;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" class="text-white h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2" />
-                                    </svg>
-                                </button>
-                    
-                                @auth
-                                    <a href="/codes/{{ $native->id }}/edit" title="Copy" class="cursor-pointer p-2 absolute top-2 right-10" style="background-color: #0d1117;">
-                                        <svg xmlns="http://www.w3.org/2000/svg" class="text-white h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
-                                        </svg>
-                                    </a>
-                                @endauth
-                    
-                                <script>
-                                    function copyCodeNative{{ $native->id }}() {
-                                        var copyText = document.getElementById("copynative-{{ $native->id }}")
-                                        navigator.clipboard.writeText(copyText.innerHTML)
-                                    }
-                                </script>
-                                
-                                <div style="display:none" id="copynative-{{ $native->id }}">{{ $native->native }}</div>
-                                <x-markdown class="show-code">
-```lua
-{!! $native->native !!}
-```
-                                </x-markdown>
-                            </div>
-                        </div>
+                        <span class="text-yellow-600">{{ $native->type }} => {{ $native->method }}</span> 
                     </div>
                 @endforeach
             </div>
