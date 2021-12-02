745469b7e0cb00c0d3b0ade10d82c389601e7b32
striderxfossility <y%^w7W%8Hc%t>
2021-12-02 16:21:51 +0100

    remove filter
    Signed-off-by: striderxfossility <y%^w7W%8Hc%t>

--- app/Http/Controllers/TweakController.php
+++ app/Http/Controllers/TweakController.php
@@ -9,13 +9,7 @@ class TweakController extends Controller
 {
     public function index() 
     {
-        $tweakgroups = TweakGroup::whereTweakGroupName('')->get()->filter(function ($value, $key) {
-            if (TweakGroup::whereName($value->name)->count() == 1) {
-                return true;
-            } else {
-                return false;
-            }
-        });
+        $tweakgroups = TweakGroup::whereTweakGroupName('')->get();
 
         return view('pages.tweakdb.index')->with('tweakgroups', $tweakgroups);
     }
