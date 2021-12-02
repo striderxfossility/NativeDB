4540eeeb8585561e408dae8c0804c91077671f15
striderxfossility <y%^w7W%8Hc%t>
2021-12-02 12:07:14 +0100

    updated to seeder and giving info
    Signed-off-by: striderxfossility <y%^w7W%8Hc%t>

--- database/seeders/TweakSeeder.php
+++ database/seeders/TweakSeeder.php
@@ -21,26 +21,68 @@ REPLACE
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\TweakGroup;
+use App\Models\TweakValue;
 
 class TweakSeeder extends Seeder
 {
     public function run()
     {
-        $timestamp  = now()->toDateTimeString();
-        
         if (!file_exists('public/tweakdb.json')) {
+            dump("Cannot find tweakdb.json, upload it to public dir");
             $this->command->error("Cannot find tweakdb.json, upload it to public dir");
         } else {
-            $string = file_get_contents(base_path('public/tweakdb.json'));
-            $string = preg_replace('/([0-9^]+):/gm', '"$1":', $string);
+            $string         = file_get_contents(base_path('public/tweakdb.json'));
+            $string         = preg_replace('/([0-9^]+):/', '"$1":', $string);
+            $string         = str_replace('\\', '/', $string);
+            $arr            = json_decode($string, true);
+            $amountOfFlats  = 21;
+            $timestamp  = now()->toDateTimeString();
 
-            $json   = json_decode($string, true);
-
-            if($json == null) {
+            if($arr == null) {
                 $this->command->error("tweakdb.json is not a valid json file");
                 dump(json_last_error());
             } else {
-                dump($json);
+                for ($i=0; $i < $amountOfFlats; $i++) { 
+                    $this->command->info('Start ' . $i . '/' . $amountOfFlats . ' tweakFlats extracting');
+                    $y = 0;
+                    $countAmounts = count($arr['flat']['keys' . $i]);
+                    foreach($arr['flat']['keys' . $i] as $key => $value)
+                    {
+                        $this->command->info('Start (' . $i . '/' . $amountOfFlats . ') => ' . $y . '/' . $countAmounts . ' tweaks extracting');
+                        $y++;
+                        $groups = explode('.', $key);
+                        $headGroup = 0;
+                        for ($x=0; $x < count($groups) - 1; $x++) { 
+                            $tweakGroup = TweakGroup::whereName($groups[$x])->whereTweakGroupId($headGroup)->first();
+
+                            if($tweakGroup != null) {
+                                $headGroup = $tweakGroup->id;
+                            } else {
+                                $headGroup = TweakGroup::insertGetId([
+                                    'tweak_group_id' => $headGroup,
+                                    'name'           => $groups[$x],
+                                    "created_at"     => $timestamp,
+                                    "updated_at"     => $timestamp,
+                                ]);
+                            }
+                        }
+
+                        $value = json_encode($arr['flat']['values' . $i][$value]);
+                        $tweakValue = TweakValue::whereTweakGroupId($headGroup)->whereName($key)->whereValue($value)->first();
+
+                        if ($tweakValue == null) {
+                            TweakValue::create([
+                                'tweak_group_id'    => $headGroup,
+                                'name'              => $key,
+                                'value'             => $value
+                            ]);
+                        } else {
+                            $tweakValue->value = $value;
+                            $tweakValue->update();
+                        }
+                    }
+                }
             }
         }
     }
